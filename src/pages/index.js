80244fa20a49ab52fc5458bf70a398d949c6e724
zachqdmi <zquinn@dminc.com>
2020-04-29 20:52:22 -0400

    newline for andrew and mc

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ const IndexPage = () => (
     We are saddened to announce that due to COVID-19 and the restrictions for events in place, we have decided to postpone our wedding celebration to August 22, 2020.<p/>
     We will be in touch with more details as the date approaches. We will be having a small private ceremony on May 30th with our immediate family and grandparents. <p/>
     We love you all and wish everyone could be there with us, but it is important that we keep everyone safe and healthy. We are looking forward to celebrating with everyone in August. <p/>
-    Love, Andrew & Mackenzie</b>
+    Love,<br/>
+    Andrew & Mackenzie</b>
     <p/>
     <Background count={0}/>
     <br />
