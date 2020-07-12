3324a5aaf11d3916b0756ac11a38f49d0ac08241
zachqdmi <zquinn@dminc.com>
2020-07-12 16:12:20 -0400

    Add RSVP

---
+++ src/components/RSVP.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const RSVP = () => (
+  <div id="RSVP">
+    <h1 style={{ fontFamily: 'cursive' }}>RSVP</h1>
+    <b>Please RSVP below if you are able to celebrate with us:</b>
+    <form name="RSVP" method="POST" data-netlify="true">
+      <b>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Number in Party (including yourself): <input type="number" min="1" max="20" name="guests"></input></label>
+        </p>
+        <p>
+          <button type="submit" style={{ borderRadius: '15px', backgroundColor: 'transparent', padding: '10px 35px' }}>Send</button>
+        </p>
+      </b>
+    </form>
+  </div>
+);
+
+export default RSVP;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Photos from '../components/photos';
-import OurStory from '../components/ourStory';
+import RSVP from '../components/RSVP';
 import TheWedding from '../components/thewedding';
 import Registry from '../components/registry';
 import Background from '../components/background';
 
 const IndexPage = () => (
   <Layout>
-    <b>Family & Friends, <br/>
-    We are saddened to announce that due to COVID-19 and the restrictions for events in place, we have decided to postpone our wedding celebration to August 22, 2020.<p/>
-    We will be in touch with more details as the date approaches. We will be having a small private ceremony on May 30th with our immediate family and grandparents. <p/>
-    We love you all and wish everyone could be there with us, but it is important that we keep everyone safe and healthy. We are looking forward to celebrating with everyone in August. <p/>
-    Love,<br/>
+    <b>Family & Friends, <br />
+    We are saddened to announce that due to COVID-19 and the restrictions for events in place, we have decided to postpone our wedding celebration to August 22, 2020.<p />
+    We will be in touch with more details as the date approaches. We will be having a small private ceremony on May 30th with our immediate family and grandparents. <p />
+    We love you all and wish everyone could be there with us, but it is important that we keep everyone safe and healthy. We are looking forward to celebrating with everyone in August. <p />
+    Love,<br />
     Andrew & Mackenzie</b>
-    <p/>
-    <Background count={0}/>
+    <p />
+    <Background count={0} />
+    <p />
+    <p />
+
+    <RSVP />
+    <hr />
+
     <br />
     {/* <BackgroundImage /> */}
     <TheWedding />
