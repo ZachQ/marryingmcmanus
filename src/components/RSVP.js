fad9222d1ee7454087c5a356fb0846e4771c5d5d
zachqdmi <zquinn@dminc.com>
2020-07-15 21:52:42 -0400

    Update to have hidden field in form for static site.

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -5,6 +5,7 @@ const RSVP = () => (
     <h1 style={{ fontFamily: 'cursive' }}>RSVP</h1>
     <b>Please RSVP below if you are able to celebrate with us:</b>
     <form name="RSVP" method="POST" data-netlify="true">
+      <input type="hidden" name="RSVP" value="contact" />
       <b>
         <p>
           <label>Your Name: <input type="text" name="name" /></label>
